2f931615820073eeccf176b33bc6ce58735e84a7
đức lê minh <lmduc2309@gmail.com>
2019-07-26 11:51:28 +0700

    Fix Footer

--- src/candidate/components/Footer.js
+++ src/candidate/components/Footer.js
@@ -6,24 +6,25 @@ export default class Footer extends Component {
   render() {
     return (
       <div>
-        <footer className="page-footer font-small grey darken-3 py-1">
+      <div className="footer-web">
             <div className="container">
               <div class="row">
                 <div class="col-md-12 py-1">
-                  <div class="mb-5 flex-center footer-social" id="fixa">
-                    <h5>Contact us: &nbsp;</h5>
+                  <div class="mb-5 footer-social" id="fixa">
+                    <span style={{fontSize: 20}}>Contact us: </span>
+
                     <NavLink to={"https://www.facebook.com/enclaveit"}><span className="icon-facebook" /></NavLink>
                     <NavLink to={"#"}><span className="icon-twitter" /></NavLink>
                     <NavLink to={"#"}><span className="icon-instagram" /></NavLink>
                     <NavLink to={"#"}><span className="icon-skype" /></NavLink>
+
+                    <h6>© 2007 - 2019 TRG-Enclave:12. All Rights Reserved. Terms and Conditions.</h6>
                   </div>
                 </div>
               </div>
-              <div className="footer-copyright text-center py-3">
-                © 2007 - 2019 TRG-Enclave:12. All Rights Reserved. Terms and Conditions.
+              
             </div>
-            </div>
-          </footer>
+          </div>
           </div>
     )
   }
