f522dd18c17ce4dbddda830f321fa9cf1a059b5c
đức lê minh <lmduc2309@gmail.com>
2019-07-29 10:17:09 +0700

    [WA-84] Refactor UI for Home page

--- src/candidate/components/Footer.js
+++ src/candidate/components/Footer.js
@@ -5,27 +5,24 @@ import { FacebookShareButton, FacebookIcon} from 'react-share';
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-      <div className="footer-web">
-            <div className="container">
-              <div class="row">
-                <div class="col-md-12 py-1">
-                  <div class="mb-5 footer-social" id="fixa">
-                    <span style={{fontSize: 20}}>Contact us: </span>
-
-                    <NavLink to={"https://www.facebook.com/enclaveit"}><span className="icon-facebook" /></NavLink>
-                    <NavLink to={"#"}><span className="icon-twitter" /></NavLink>
-                    <NavLink to={"#"}><span className="icon-instagram" /></NavLink>
-                    <NavLink to={"#"}><span className="icon-skype" /></NavLink>
-
-                    <h6>© 2007 - 2019 TRG-Enclave:12. All Rights Reserved. Terms and Conditions.</h6>
+      <footer>
+  <div className="footer" id="footer">
+  
+    <div className="container">
+    <div class="col-md-12 py-1">
+                  <div class="mb-5 text-center footer-social" id="fixa">
+                    <span>Contact us: &nbsp;</span>
+                    <NavLink to={"https://www.facebook.com/enclaveit"} className="facebook"><span className="icon-facebook" /></NavLink>
+                    <NavLink to={"#"} className="twitter"><span className="icon-twitter" /></NavLink>
+                    <NavLink to={"#"} className="skype"><span className="icon-skype" /></NavLink>
                   </div>
                 </div>
-              </div>
-              
-            </div>
-          </div>
-          </div>
+      <p className="text-center textbottom"> © 2007 - 2019 TRG-Enclave:12. All Rights Reserved. Terms and Conditions. </p>
+    </div>
+  </div>
+  {/*/.footer-bottom*/} 
+</footer>
+
     )
   }
 }
