2aecc971f884c657e59452eec5be30cabbb89378
hoangndm <40530388+hoangndm@users.noreply.github.com>
2019-07-31 15:27:53 +0700

    Update InterviewsPage.js

--- src/admin/pages/InterviewsPage.js
+++ src/admin/pages/InterviewsPage.js
@@ -8,7 +8,8 @@ import Pagination from '../components/Pagination.js';
 import { ClipLoader } from 'react-spinners';
 import $ from 'jquery';
 const styleFont = {
-  fontSize: '200%'
+  fontSize: '200%',
+  fontWeight: 'bold',
 };
 const styleCard = {
   width: '90%',
