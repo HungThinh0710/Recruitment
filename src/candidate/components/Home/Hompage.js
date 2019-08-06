e0add3eb68a2a0321c302d84a372f155efe6b3cd
đức lê minh <lmduc2309@gmail.com>
2019-08-06 15:04:48 +0700

    active pagination and filter

--- src/candidate/components/Home/Hompage.js
+++ src/candidate/components/Home/Hompage.js
@@ -28,6 +28,9 @@ export default class Homepage extends Component {
       designer: false,
       tester: false,
       developer: false,
+      status: '',
+      activeItem: 1,
+      
 
     };
     this.handlePageChange = this.handlePageChange.bind(this);
@@ -65,6 +68,7 @@ export default class Homepage extends Component {
     return str;
   }
   async componentDidMount() {
+   
     let headers = {
       "Content-Type": "application/json",
       "Accept": "application/json",
@@ -121,10 +125,11 @@ export default class Homepage extends Component {
     });
   }
   
-  handlePageChange(pageNumber) {
-    
-    const {keyword} = this.state;
-    const {location} = this.state;
+  handlePageChange(pageNumber, keyword, location, status) {
+    keyword = this.state.keyword;
+    location = this.state.location;
+    status = this.state.status;
+    // const {keyword, location, status} = this.state;
     let headers = {
       "Content-Type": "application/json",
       "Accept": "application/json",
@@ -133,10 +138,10 @@ export default class Homepage extends Component {
       "keyword": keyword,
       "position": "",
       "location": location,
-      "experience": "",
+      "catelogy": "",
+      "status": status,
       "orderby": "asc"
     }
-    // this.setState({activePage: pageNumber});
     var url = 'https://api.enclavei3dev.tk/api/article-web?page=' + pageNumber;
     fetch(url, {
       method: 'POST',
@@ -148,7 +153,7 @@ export default class Homepage extends Component {
           currentPage: data.currentPage,
           totalItems: data.total,
           listjob: data.data,
-          activePage: pageNumber,
+          activePage: data.current_page,
         }); 
       });
       
@@ -166,10 +171,18 @@ export default class Homepage extends Component {
     })
     
 }
+  handleActive(index) {
+    this.setState({
+      activeItem: index
+    })
+  }
   
 
   render() {
     const {active} = this.state;
+    
+
+    
     return (
       <section id="Home">
         <div className="site-wrap" >
@@ -216,13 +229,13 @@ export default class Homepage extends Component {
                       </p>
                       <ul class="list-group">
                         <li class="list-group-item">
-                          <NavLink to={"#"}>Why join Enclave</NavLink>
+                          <NavLink className="item" to={"#"}>Why join Enclave</NavLink>
                         </li>
                         <li class="list-group-item">
-                          <NavLink to={"#"}>Working environment</NavLink>
+                          <NavLink className="item" to={"#"}>Working environment</NavLink>
                         </li>
                         <li class="list-group-item">
-                          <NavLink to={"#"}>Flat model</NavLink>
+                          <NavLink className="item" to={"#"}>Flat model</NavLink>
                         </li>
                       </ul>
                     </li>
@@ -230,23 +243,25 @@ export default class Homepage extends Component {
                       <p class="list-group-title">
                         Job opening
                       </p>
-                      <ul  class="site-nemu list-group navbar-nav" >
-                        <li className="list-group-item"  name="All">
-                          <NavLink to="/" exact  onClick={()=>this.handleFilter('')}>All</NavLink>
+                      
+                      <ul  class="site-nemu list-group navbar-nav" id="myLink">
+                        <li key={1} className={this.state.activeItem === 1 ? 'list-group-item active' : 'list-group-item'} onClick={this.handleActive.bind(this, 1)}  onClick={()=>this.handleFilter('')} name="All">
+                          <Link key={1} className={this.state.activeItem === 1 ? 'item active' : 'item'} onClick={this.handleActive.bind(this, 1)}  to="/" exact  >All</Link>
                         </li>
-                        <li  className="list-group-item" name="internship">
-                          <NavLink  to="/" exact  onClick={()=>this.handleFilter('Internship')}>Internship</NavLink>
+                        <li key={2} className={this.state.activeItem === 2 ? 'list-group-item active' : 'list-group-item'} onClick={this.handleActive.bind(this, 2)} onClick={()=>this.handleFilter('Internship')} name="internship">
+                          <Link key={2} className={this.state.activeItem === 2 ? 'item active' : 'item'} onClick={this.handleActive.bind(this, 2)}  to="/" exact  >Internship</Link>
                         </li>
-                        <li className="list-group-item" name="designer">
-                          <NavLink to="/" exact  onClick={()=>this.handleFilter('Desginer')}>Designer</NavLink>
+                        <li key={3} className={this.state.activeItem === 3 ? 'list-group-item active' : 'list-group-item'} onClick={this.handleActive.bind(this, 3)} onClick={()=>this.handleFilter('Desginer')} name="designer">
+                          <Link key={3} className={this.state.activeItem === 3 ? 'item active' : 'item'} onClick={this.handleActive.bind(this, 3)} to="/" exact  >Designer</Link>
                         </li>
-                        <li className="list-group-item" name="tester">
-                          <NavLink to="/" exact  onClick={()=>this.handleFilter('Tester')}>Tester</NavLink>
+                        <li key={4} className={this.state.activeItem === 4 ? 'list-group-item active' : 'list-group-item'} onClick={this.handleActive.bind(this, 4)} onClick={()=>this.handleFilter('Tester')} name="tester">
+                          <Link key={4} className={this.state.activeItem === 4 ? 'item active' : 'item'} onClick={this.handleActive.bind(this, 4)} to="/" exact  >Tester</Link>
                         </li>
-                        <li className="list-group-item" name="developer">
-                          <NavLink to="/" exact  onClick={()=>this.handleFilter('Developer')}>Developer</NavLink>
+                        <li key={5} className={this.state.activeItem === 5 ? 'list-group-item active' : 'list-group-item'} onClick={this.handleActive.bind(this, 5)} onClick={()=>this.handleFilter('Developer')} name="developer">
+                          <Link key={5} className={this.state.activeItem === 5 ? 'item active' : 'item'} onClick={this.handleActive.bind(this, 5)} to="/" exact  >Developer</Link>
                         </li>
                       </ul>
+                      
                     </li>
                   </ul>
                 </aside>
@@ -256,7 +271,7 @@ export default class Homepage extends Component {
                       <div class="panel-title">Search & apply</div>
                       <div class="panel-body wrap-form border">
                         <form method="post" className="search-jobs-form">
-                          <div className="row mb-5">
+                          <div className="row mb-5 Searchtype">
                             <div className="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
                               <label>Position</label>
                               <input name="keyword" value={this.state.keyword} onChange={this.handleChange} type="text" className="form-control form-control-lg-2" ref="search" placeholder="Search" />
@@ -270,12 +285,12 @@ export default class Homepage extends Component {
                               </select>
                             </div>
                             <div className="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
-                              <label>Job category</label>
-                              <select className="form-control">
-                                <option>All</option>
-                                <option>Part Time</option>
-                                <option>Full Time</option>
-                                <option>Freelancer</option>
+                              <label>Status</label>
+                              <select name="status" value={this.state.status} onChange={this.handleChange} className="form-control">
+                                <option value="">All</option>
+                                <option value="Part-time">Part Time</option>
+                                <option value="Full-time">Full Time</option>
+                                {/* <option value="freelancer">Freelancer</option> */}
                               </select>
                             </div>
                             <div className="col-12 ml-auto col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
@@ -294,6 +309,7 @@ export default class Homepage extends Component {
                             <th>Position</th>
                             <th>Category</th>
                             <th>Location</th>
+                            <th>Status</th>
                             <th>Day expired</th>
                           </thead>
                           <tbody class="transistion">
@@ -325,6 +341,15 @@ export default class Homepage extends Component {
                                     <p class="location-id">
                                       <p class="location-title">
                                         {p.job.address}
+                                      </p>
+                                    </p>
+                                  </div>
+                                </td>
+                                <td data-th="Status">
+                                  <div>
+                                    <p class="status-id">
+                                      <p class="status-title">
+                                        {p.job.status}
                                       </p>
                                     </p>
                                   </div>
